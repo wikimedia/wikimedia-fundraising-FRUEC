03d24d09e2b8ad5fdb1a8f50f5c30a176f8c0b54
Andrew Green <andrew.green.df@gmail.com>
2018-08-31 13:30:27 -0500

    Print a friendly message confirming config
    Bug: T195594
    Change-Id: I018496875317bdaf66f362bfe1d7d1e774b7e281

--- fr_user_event_consumer/event_type.py
+++ fr_user_event_consumer/event_type.py
@@ -1,8 +1,16 @@
 from enum import Enum
 
 class EventType(Enum):
-    CENTRAL_NOTICE = ( 'banner' )
-    LANDING_PAGE = ( 'landingpage' )
+    # output_name coordinates with the names of event types used for the command-line
+    # option, where they just seems better without dashes or underscores...
+    CENTRAL_NOTICE = ( 'banner', 'centralnotice' )
+    LANDING_PAGE = ( 'landingpage', 'landingpage' )
 
-    def __init__( self, legacy_key ):
+
+    def __init__( self, legacy_key, output_name ):
         self.legacy_key = legacy_key
+        self.output_name = output_name
+
+
+    def __str__( self ):
+        return self.output_name
